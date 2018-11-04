147aac18e49bad8a3c2515c5bdd5f95f810a95f3
Kevin Trejos <Kevin Trejos@KTC>
2018-11-04 11:46:56 -0600

    Metodos main

--- LaboratorioHerencia/src/laboratorioherencia/TestZonasProtegidas.java
+++ LaboratorioHerencia/src/laboratorioherencia/TestZonasProtegidas.java
@@ -11,6 +11,19 @@ package laboratorioherencia;
  */
 public class TestZonasProtegidas {
     public static void main(String[] args) {
-  
+        ParqueNacional parque = new ParqueNacional("Carrillo", "Guanacaste",10, 15, 20);
+        parque.calculateAmount();
+        NoCulturalMonument monumentoNoCultural = new NoCulturalMonument("No cultural", "San Pepe",15, 5);
+        monumentoNoCultural.calculateAmount();
+        System.out.println(monumentoNoCultural.toString());
+        PatrimonioCultural patrimonio = new PatrimonioCultural("Guayabo", "No sé", 20, 12, 2010000000);
+        patrimonio.calculateAmount();
+        System.out.println(patrimonio);
+        PropiedadEstado estado = new PropiedadEstado("jsndd", "Alajuela", 149898);
+        estado.calculateAmount();
+        System.out.println(estado);
+        PropiedadPrivada privada = new PropiedadPrivada("privado", "Guanacaste", 105060);
+        privada.calculateAmount();
+        System.out.println(privada);
     }
 }
